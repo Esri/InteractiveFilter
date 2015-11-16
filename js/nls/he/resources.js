a0174357bb2acbe48da13743be7bf3f55bf693bc
Kelly Hutchins <khutchins@esri.com>
2015-11-16 15:15:11 -0800

    version update

--- js/nls/he/resources.js
+++ js/nls/he/resources.js
@@ -9,7 +9,8 @@
       filterInstructions: "בצע סינון לשכבה על ידי הגדרת ערכים.",
       filterOr: "כל אחד מהביטויים הבאים חייב לענות על קריטריון true.",
       filterAnd: "כל הביטויים הבאים חייבים לענות על קריטריון true.",
-      filterNo:  "Web map does not contain any interactive filters. View the <a target='_blank' href='http://doc.arcgis.com/en/arcgis-online/use-maps/apply-filters.htm'>Apply Filter</a> help topic for details on how to create a filter in your web map.",
+      filterNo:  "Web map does not contain any interactive filters. View the ${link} help topic for details on how to create a filter in your web map.",
+      filterLink: "http://doc.arcgis.com/en/arcgis-online/use-maps/apply-filters.htm",
       errors:{
         message: "בעיה ביצירת אפליקצית מסננים"
       }
