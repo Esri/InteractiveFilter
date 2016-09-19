ba5d0d559eaa9341e1b69841226c09e5f3ddc46a
Kelly Hutchins <khutchins@esri.com>
2016-09-19 11:05:54 -0700

    4.3 updates

--- js/nls/sr/resources.js
+++ js/nls/sr/resources.js
@@ -1,17 +1,17 @@
 define({
   "map": {
-    "error": "Kan kaart niet maken"
+    "error": "Kreiranje mape nije moguće"
   },
   "viewer": {
     "content_title": "Filter",
-    "button_text": "Toepassen",
-    "filterInstructions": "Filter de laag door waarden op te geven.",
-    "filterOr": "Minstens één van de volgende expressies moet waar zijn.",
-    "filterAnd": "Alle volgende expressies moeten waar zijn.",
+    "button_text": "Primeni",
+    "filterInstructions": "Filtrirajte sloj po određenim vrednostima.",
+    "filterOr": "Bilo koji od narednih izraza mora da bude tačan.",
+    "filterAnd": "Svi naredni izrazi moraju da budu tačni.",
     "filterNo": "Web map does not contain any interactive filters. View the ${link} help topic for details on how to create a filter in your web map.",
     "filterLink": "http://doc.arcgis.com/en/arcgis-online/use-maps/apply-filters.htm",
     "errors": {
-      "message": "Probleem bij het maken van de applicatie van de filter"
+      "message": "Problem pri kreiranju filtera aplikacije"
     }
   },
   "legend": {
